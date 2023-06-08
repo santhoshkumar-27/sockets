f802bdd7431195b1c04bc1595b211b3356b620e4
santhoshkumar <its.santhoshkumar@outlook.com>
2023-06-08 06:43:15 +0530

    real time chat app

--- app/app.js
+++ app/app.js
@@ -1,14 +1,9 @@
-const socket = new WebSocket('ws://localhost:8080');
+const socket = io('ws://localhost:8080');
 
-socket.onmessage = ({ data }) => {
+socket.on('message', data => {
     console.log('message from backed', data)
     createElement(data)
-}
-
-// document.getElementById('click', ())
-// document.querySelector('#click').onclick = () => {
-//     socket.send('hello')
-// }
+})
 
 function sendMessage() {
     // Get the input value
@@ -31,20 +26,3 @@ function createElement(message) {
     var ul = document.getElementById("messageList");
     ul.appendChild(li);
 }
-/*
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <button id="click">click me to call websockets</button>
-</body>
-
-</html>
-*/
--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
-const SebSocket = require('ws');
-const server = new SebSocket.Server({
-    port: '8080'
+// const SebSocket = require('ws');
+// const server = new SebSocket.Server({
+//     port: '8080'
+// })
+
+// server.on('connection', (socket) => {
+
+//     socket.on('message', (message) => {
+
+//         socket.send(`Roger that ${message}`);
+//     })
+// })
+
+const http = require('http').createServer();
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*'
+    }
 })
 
-server.on('connection', (socket) => {
+io.on('connection', (socket) => {
+    console.log('a user is connected');
 
     socket.on('message', (message) => {
-
-        socket.send(`Roger that ${message}`);
+        console.log('from front end', message);
+        io.emit(`message`, message)
     })
 })
+
+http.listen(8080, () => console.log('listening http://localhost:8080'))
